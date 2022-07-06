61f69dd8607cc6eacb92394ae7c86564d22c4e3a
RhiH <rhi.herbert@gmail.com>
2022-07-06 20:51:08 +0000

    send updates to worksheet.

--- run.py
+++ run.py
@@ -12,8 +12,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
 def get_sales_data():
     """
     Get sales figures input from the user.
@@ -49,5 +47,17 @@ def validate_data(values):
         return False
     
     return True
-    
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
